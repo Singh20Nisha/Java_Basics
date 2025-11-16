7b21ca397823793dea155fa831e3f78faed5cd0c
Nisha <nisha14kumari2006@gmail.com>
2025-11-16 20:11:24 +0530

    Added second program

---
+++ second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String[] args) {
+        double pi  = 3.14;
+        int r = 25;
+        double area = pi*r*r;
+        System.out.println("Area of a circle is =");
+        System.out.println(area);
+
+    }
+}
