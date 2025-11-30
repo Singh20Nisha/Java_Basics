37f780cd8a494ff9b12d753e2aaee74e03e36759
Nisha <nisha14kumari2006@gmail.com>
2025-11-30 19:20:44 +0530

    Added pattern1

---
+++ Pattern1.java
@@ -0,0 +1,21 @@
+//***** 
+//*****
+//*****
+//*****
+//*****
+public class Pattern1 {
+ static void pattern1(int N) {
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+ }
+ public static void main(String[] args) {
+    int N = 5;
+    pattern1(N);
+ }
+        
+    }
+
